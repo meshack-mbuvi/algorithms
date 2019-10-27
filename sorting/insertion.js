81068b4ecba216215cf7d3c50c32c88248ae541f
Meshack Mbuvi <meshmbuvi@gmail.com>
2019-10-28 00:33:29 +0300

    Add insertion sort algorithm implementation

---
+++ sorting/insertion.js
@@ -0,0 +1,21 @@
+function insertionSort(array) {
+  const n = array.length;
+
+  for (let i = 1; i < n; i++) {
+    let el = array[i];
+
+    // loop to perform the insertion
+    for (let j = i; j >= 0; j--) {
+      if (array[j] > el) {
+        array[j + 1] = array[j];
+        array[j] = el;
+      }
+    }
+  }
+  return array;
+}
+
+const test = [9, 8, 7, 6, 5];
+
+const result = insertionSort(test);
+console.log({ result });
